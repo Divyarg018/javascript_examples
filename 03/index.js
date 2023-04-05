0a72e3cb65f3bd0d39bc7b074acd7b34a114f9ac
Divya R <kwrdivyaraj100@gmail.com>
2023-04-05 23:25:29 +0530

    variables in JS

---
+++ 03/index.js
@@ -0,0 +1,17 @@
+console.log('tut3');
+// Variables in js
+// var, let, const
+var name = 'harry';
+var channel;
+var marks = 3454;
+marks = 0;
+channel = 'CodeWithHarry'
+console.log(name, channel, marks);
+// Rules for creating JavaScript Variables
+/*
+1. Cannot start with numbers
+2. Can start with letter, numbers, _ or $
+3. Are case sensitive
+*/
+var city = 'Delhi';
+console.log(city);
