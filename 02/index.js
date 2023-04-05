dee0ba3fc9b919dc09c3bd9abc231c1b9eb1b865
Divya R <kwrdivyaraj100@gmail.com>
2023-04-05 22:44:33 +0530

    console methods

---
+++ 02/index.js
@@ -0,0 +1,7 @@
+console.time('Your code Took');
+console.log('Hello console');
+console.log(4+34);
+console.log(34);
+console.log(true);
+console.log([34,2,1,2]);
+console.log({harry: 'this', marks:34});
