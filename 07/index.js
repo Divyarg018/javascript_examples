618ac3e2ec081685521c9271c9bec3ff9f10954f
Divya R <kwrdivyaraj100@gmail.com>
2023-04-07 11:43:33 +0530

    07 arrays in JS

---
+++ 07/index.js
@@ -0,0 +1,16 @@
+console.log('We are in tut7.js and lets discuss about arrays');
+let marks = [34, 23, 24, 93 ,73, 25];
+const fruits = ['Orange', 'Apple', 'Pineapple'];
+const mixed = ['str', 89, [3, 5]];
+
+const arr = new Array(23,123,21, 'Orange');
+// console.log(marks);
+// console.log(mixed);
+// console.log(fruits[1]);
+
+// console.log(arr.length);
+// console.log(Array.isArray('dfdf'));
+arr[0] = 'harry';
+let arrelement = arr[0];
+// console.log('element :', arrelement);
+// console.log(arr);
