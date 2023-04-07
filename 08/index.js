ba20c4d16cce3a5df531645dc952a7c721013e96
Divya R <kwrdivyaraj100@gmail.com>
2023-04-07 13:20:26 +0530

    08 switches in JS

--- 08/index.js
+++ 08/index.js
@@ -31,3 +31,20 @@ const doesDrive = false;
 // }
 
 // console.log(age==45? 'Age is 45': 'Age is not 45');
+switch (age) {
+    case 18:
+        console.log("You are 18");
+        break;
+
+    case 28:
+        console.log("You are 28");
+        break;
+
+    case 38:
+        console.log("You are 38");
+        break;
+
+    default:
+        console.log("You are unknown age");
+        break;
+}         
