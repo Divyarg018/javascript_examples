80ee69867fb98e17f64bd0da470d60c9fe9e7e2f
Divya R <kwrdivyaraj100@gmail.com>
2023-04-08 19:27:27 +0530

    11 JS window object

---
+++ 11/index.js
@@ -0,0 +1,6 @@
+console.log('This is tut 11');
+let a = window.document;
+// alert('hello harry');
+// a = prompt('This will destroy your computer. Type your name');
+
+// a = confirm('Are you sure you want to delete this page?');
