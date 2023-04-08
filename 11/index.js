667ba2bfd28fbc6a9c324bb628d47d1077f1ca07
Divya R <kwrdivyaraj100@gmail.com>
2023-04-08 20:02:25 +0530

    11 window object methods

--- 11/index.js
+++ 11/index.js
@@ -4,3 +4,10 @@ let a = window.document;
 // a = prompt('This will destroy your computer. Type your name');
 
 // a = confirm('Are you sure you want to delete this page?');
+
+a = window.innerHeight;
+a = innerWidth;
+a = scrollY;
+a = location.toString();
+a = window.history;
+console.log(a);  
