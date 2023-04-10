482ef376b5243cb17b4a5f12b3c0c10e5194101b
Divya R <kwrdivyaraj100@gmail.com>
2023-04-10 09:13:28 +0530

    16 create element

---
+++ 16/index.js
@@ -0,0 +1,11 @@
+console.log('this is tut 16');
+let element = document.createElement('li');
+let text = document.createTextNode('I am a text node');
+element.appendChild(text)
+
+// Add a class name to the li element
+element.className = 'childul';
+element.id = 'createdLi';
+element.setAttribute('title', 'mytitle');
+// element.innerText = '<b>Hello this is created by harry</b>';
+// element.innerHTML = '<b>Hello this is created by harry</b>';
