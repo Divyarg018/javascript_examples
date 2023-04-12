25782eea305edc6fd4678d86841cf1ad303b134b
Divya R <kwrdivyaraj100@gmail.com>
2023-04-12 19:50:20 +0530

    23 math object

---
+++ 23/index.js
@@ -0,0 +1,16 @@
+console.log("Welcome to tutorial number 23");
+let x = 3;
+let y = 6;
+let z;
+z = x+y;
+z = x-y;
+z = x*y;
+z = x/y;
+
+// Exploring the Math object
+z = Math;
+z = Math.PI;
+z = Math.E;
+z = Math.round(5.4);
+z = Math.ceil(5.3);
+console.log(z);
