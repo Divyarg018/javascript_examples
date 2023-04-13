1e9f3535e1f9aa5fa665b8e01e24702e4cd2e779
Divya R <kwrdivyaraj100@gmail.com>
2023-04-13 20:05:36 +0530

    25 added eventListener to divElem

--- 25/index.js
+++ 25/index.js
@@ -33,3 +33,17 @@ let first = document.getElementById('myfirst');
 container.insertBefore(divElem, first);
 
 console.log(divElem, container, first)
+// add event listener to the divElem
+divElem.addEventListener('click', function () {
+    let noTextAreas = document.getElementsByClassName('textarea').length;
+    if(noTextAreas ==0){
+    let html = elem.innerHTML;
+    divElem.innerHTML = ` <textarea class="textarea form-control" id="textarea" rows="3"> ${html}</textarea>`;
+    }
+    //listen for the blur event on textarea
+    let textarea = document.getElementById('textarea');
+    textarea.addEventListener('blur', function() {
+        elem.innerHTML = textarea.value;
+        localStorage.setItem('text', textarea.value);
+    })
+});
