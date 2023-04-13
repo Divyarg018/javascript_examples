7dff847e12160c08b2463d3a66e49061d4b33d10
Divya R <kwrdivyaraj100@gmail.com>
2023-04-13 19:04:09 +0530

    25 created new element and added text to that element

---
+++ 25/index.js
@@ -0,0 +1,20 @@
+console.log('This is tutorial 25')
+/*
+You have to create a div and inject it into the page which contains a heading.
+whenever someone clicks on the div, it should be converted into an editable item. whenever user clicks away (blur). save the note into the local storage.
+
+*/
+
+// Create a new element
+let divElem = document.createElement('div');
+
+// Add text to that created element
+let val = localStorage.getItem('text');
+let text;
+if (val==null){
+ text = document.createTextNode('This is my element. click to edit it');
+}
+else{
+    text = document.createTextNode(val);
+}
+divElem.appendChild(text);
