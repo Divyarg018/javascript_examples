ed077bc34719e6ca3d54f6c547a42ff5b09cd718
Divya R <kwrdivyaraj100@gmail.com>
2023-04-15 23:06:38 +0530

    31 extending a class

--- 31/index.js
+++ 31/index.js
@@ -10,4 +10,29 @@ class Employee {
     slogan(){
         return `I am ${this.name} and this company is the best`;
     }
+
+joiningYear(){
+    return 2020 - this.experience;
+}
+
+static add(a, b){
+    return a + b;
+}
+}
+
+class Programmer extends Employee{
+constructor(givenName, givenExperience, givenDivision, language, github){
+    super(givenName, givenExperience, givenDivision);
+    this.language = language;
+    this.github = github;
+}
+
+favoriteLanguage(){
+    if (this.language == 'python'){
+        return 'Python';
+    }
+    else{
+        return 'JavaScript';
+    }
+}
 }
