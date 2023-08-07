0c49e3c5349f3df4d62b50c5fd8687866a54f678
Divya R <kwrdivyaraj100@gmail.com>
2023-08-07 10:16:51 +0530

    synchronus program example

--- 0K/index.js
+++ 0K/index.js
@@ -1,17 +1,33 @@
-let balajiBath = function() {
+// let balajiBath = function() {
+//     console.log(`\nBalji woke up`);
+//     console.log(`Balji is Bathing`);
+//     console.log(`Balji is Ready`);
+// }
+
+// let kishanBath = function() {
+//     console.log(`\nKishan woke up`);
+//     console.log(`Kishan is Bathing`);
+//     console.log(`Kishan is Ready`);
+// }
+
+// balajiBath();
+// kishanBath();
+
+// kishanBath();
+// balajiBath();
+
+
+let balajiBath = function () {
     console.log(`\nBalji woke up`);
     console.log(`Balji is Bathing`);
     console.log(`Balji is Ready`);
 }
 
-let kishanBath = function() {
-    console.log(`\nKishan woke up`);
-    console.log(`Kishan is Bathing`);
-    console.log(`Kishan is Ready`);
+let kishanBath = function () {
+    setTimeout(() => { console.log(`\nKishan woke up`) }, 2000);
+    setTimeout(() => { console.log(`Kishan is Bathing`) }, 3000);
+    setTimeout(() => { console.log(`Kishan is Ready`) }, 5000);
 }
 
-balajiBath();
 kishanBath();
-
-kishanBath();
-balajiBath();
+setTimeout(balajiBath, 7000);
