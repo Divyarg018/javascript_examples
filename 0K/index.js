7a8d402133d8f21e15485dbe023acd9ea56fdc68
Divya R <kwrdivyaraj100@gmail.com>
2023-08-07 10:11:51 +0530

    synchronous program

---
+++ 0K/index.js
@@ -0,0 +1,17 @@
+let balajiBath = function() {
+    console.log(`\nBalji woke up`);
+    console.log(`Balji is Bathing`);
+    console.log(`Balji is Ready`);
+}
+
+let kishanBath = function() {
+    console.log(`\nKishan woke up`);
+    console.log(`Kishan is Bathing`);
+    console.log(`Kishan is Ready`);
+}
+
+balajiBath();
+kishanBath();
+
+kishanBath();
+balajiBath();
