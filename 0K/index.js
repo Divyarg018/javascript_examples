54bb012d08543eb55d0332235ab4cde9643d60ff
Divya R <kwrdivyaraj100@gmail.com>
2023-08-07 11:15:57 +0530

    requiremnts for callback hell complete

--- 0K/index.js
+++ 0K/index.js
@@ -35,18 +35,60 @@
 
 
 
-let balajiBath = function () {
-    console.log(`\nBalji woke up`);
-    console.log(`Balji is Bathing`);
-    console.log(`Balji is Ready`);
+// let balajiBath = function () {
+//     console.log(`\nBalji woke up`);
+//     console.log(`Balji is Bathing`);
+//     console.log(`Balji is Ready`);
+// }
+
+// let kishanBath = function (signalHim) {
+//     setTimeout(() => { console.log(`\nKishan woke up`) }, 2000);
+//     setTimeout(() => { console.log(`Kishan is Bathing`) }, 3000);
+//     setTimeout(() => { console.log(`Kishan is Ready`) }, 5000);
+// }
+
+// setTimeout(signalHim, 7000);
+
+// kishanBath(balajiBath);// previously balaji taking bath was optional
+
+/* why call back fucntion?
+
+1.Js is an event driven language.
+2.Instaed of waiting for a response from a pending event, before movong on,js will keep ececuting other events.
+3.In the meantime js will keeo watch on previous pending events, and will ec=xecute the same at an appropriate time.
+
+taht si why: call back functions are a way to make sure certain code doesnt execute until other has already finished execution.(programmers juagad)
+
+In Ashynchronous programing language like js: callBack functions are absolute necessity*/
+
+
+const profiles ={
+    'balaji': {
+        fullname: 'Balaji KR',
+        age: 35,
+        married: true,
+        hobbies: ['Defence', 'Finance', 'Foreign Affairs']
+    },
+
+    'seema': {
+        fullname: 'Seema H',
+        age: 30,
+        married: true,
+        hobbies: ['Cooking', 'Painting', 'Art & Craft']
+    },
+
+    'kishan': {
+        fullname: 'Kishan KB',
+        age: 3,
+        married: false,
+        hobbies: ['football', 'video games']
+    },
+
+    'rajeev': {
+        fullname: 'Rajeevalochana KV',
+        age: 70,
+        married: true,
+        hobbies: ['watching Tv']
+    }
+
 }
-
-let kishanBath = function (signalHim) {
-    setTimeout(() => { console.log(`\nKishan woke up`) }, 2000);
-    setTimeout(() => { console.log(`Kishan is Bathing`) }, 3000);
-    setTimeout(() => { console.log(`Kishan is Ready`) }, 5000);
-}
-
-setTimeout(signalHim, 7000);
-
-kishanBath(balajiBath);// previously balaji taking bath was optional
