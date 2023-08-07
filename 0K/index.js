0f1f0ad540b4400b7842f36430eee57349ddb9a5
Divya R <kwrdivyaraj100@gmail.com>
2023-08-07 10:21:17 +0530

    playing with setTimeout

--- 0K/index.js
+++ 0K/index.js
@@ -17,17 +17,36 @@
 // balajiBath();
 
 
+// let balajiBath = function () {
+//     console.log(`\nBalji woke up`);
+//     console.log(`Balji is Bathing`);
+//     console.log(`Balji is Ready`);
+// }
+
+// let kishanBath = function () {
+//     setTimeout(() => { console.log(`\nKishan woke up`) }, 2000);
+//     setTimeout(() => { console.log(`Kishan is Bathing`) }, 3000);
+//     setTimeout(() => { console.log(`Kishan is Ready`) }, 5000);
+// }
+
+// kishanBath();
+// setTimeout(balajiBath, 7000);
+
+
+
+
 let balajiBath = function () {
     console.log(`\nBalji woke up`);
     console.log(`Balji is Bathing`);
     console.log(`Balji is Ready`);
 }
 
-let kishanBath = function () {
+let kishanBath = function (signalHim) {
     setTimeout(() => { console.log(`\nKishan woke up`) }, 2000);
     setTimeout(() => { console.log(`Kishan is Bathing`) }, 3000);
     setTimeout(() => { console.log(`Kishan is Ready`) }, 5000);
 }
 
-kishanBath();
-setTimeout(balajiBath, 7000);
+setTimeout(signalHim, 7000);
+
+kishanBath(balajiBath);// previously balaji taking bath was optional
