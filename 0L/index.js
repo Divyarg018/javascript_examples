f8cbbb4601459d4991fe7650ef7d923b70dbe942
Divya R <kwrdivyaraj100@gmail.com>
2023-08-08 10:48:43 +0530

    async and wait

---
+++ 0L/index.js
@@ -0,0 +1,15 @@
+// Async and Wait
+
+// function balaji(){
+//     return 'balaji';
+// }
+
+// let b = balaji();
+// console.log(b); // returning a text
+
+ async function balaji(){
+    return 'balaji';
+}
+
+let b = balaji();
+console.log(b); // returning a text
