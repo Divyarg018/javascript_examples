308cb9fa6aea672feed36fb913694525fdf5bb17
Divya R <kwrdivyaraj100@gmail.com>
2023-08-16 11:54:20 +0530

    JavaScript ISO Dates

---
+++ 0M/index.js
@@ -0,0 +1,37 @@
+/* Events and JSON
+
+1. what si string? It is a representation of a information on character set.
+2. The below represents a string common in any cmpouter language(enclosed with quotes)
+
+eg. "Hello World"  // string
+
+3. If we expand same string to some readable format.
+
+'name=Amar id =101 score=90' // string
+
+In js we can represent this string with a clear format.
+
+let str = `{name: Amar, id =101, score=90}` // string JSON
+
+JS Object Notation:
+
+The above code will run if we try to access values using dot operator it says undefined.
+
+Pleasenote varuable "str" is a plain string. To access values we need to represent it as JSOn.
+
+*/
+
+// let str = "{name: Amar, id: 101, score: 90}"
+// console.log(str.obj);//undefined
+
+
+
+// let obj={
+//     name: 'Amar',
+//     id: 101,
+//     score: 90
+// }
+// console.log(obj.id);// o/p: 101
+
+
+
