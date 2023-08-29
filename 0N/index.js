0964f54b61dd071aed4deb5243363537bc36abd4
Divya R <kwrdivyaraj100@gmail.com>
2023-08-29 19:32:26 +0530

    JS math

--- 0N/index.js
+++ 0N/index.js
@@ -43,18 +43,97 @@
 
 
 
+// let animal = {
+//     eats: true,
+//     walk() {
+
+//     }
+// };
+
+// let rabbit = {
+//     __proto__: animal
+// };
+
+// rabbit.walk =function() {
+//     alert("Rabbit! Bounce-bounce");
+
+// };
+
+
+// let user = {
+//     name: "John",
+//     surname: "Smith",
+
+//     set fullName(value) {
+//         [this.name, this.surname] = value.split(" ");
+//     },
+
+//     get fullName() {
+//         return `${this.name} ${this.surname}`;
+
+// }
+// };
+
+// let admin = {
+//     __proto__: user,
+//     isAdmin: true
+// };
+
+
+// admin fullName = "Alice Cooper";
+
+
+// let father = {
+//     name: "Ramesh",
+//     city: "Vijaynagar",
+//     showInfo: function () {
+//         console.log(`My name is ${this.name} from ${this.city}`);
+//     }
+
+//    /* showInfo() {
+//     console.log(`My name is ${this.name} from ${this.city}`);
+//     } */
+
+// }
+
+// let son = {
+//     name: "Suresh"
+// }
+
+// // never do this
+// son.__proto__ = father;
+
+// let animal = {
+//         eats: true
+//     };
+
+//     let rabbit = {
+//         jumps: true,
+//         __proto__:animal
+//     };
+
+
+
+// alert(Object.keys(rabbit)); // jumps
+
+// for(let prop in rabbit) alert(prop);// jumps, then eats
+
+
 let animal = {
-    eats: true,
-    walk() {
-    
-    }
+    eats: true
 };
 
 let rabbit = {
+    jumps: true,
     __proto__: animal
 };
 
-rabbit.walk =function() {
-    alert("Rabbit! Bounce-bounce");
-    
-};
+for(let prop in rabbit) {
+    let isOwn = rabbit.hasOwnProperty(prop);
+    if(isOwn){
+        alert(`Our: ${prop}`);
+    }
+    else{
+        (`Inheritance: ${prop}`);
+    }
+}
